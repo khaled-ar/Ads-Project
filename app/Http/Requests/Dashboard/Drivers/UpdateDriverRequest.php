70f539f995e84345d7207f861c5589d82c1f1ed6
khaled-ar <khaled.ar.work@gmail.com>
2026-01-27 02:58:38 -0800

    Driver

--- app/Http/Requests/Dashboard/Drivers/UpdateDriverRequest.php
+++ app/Http/Requests/Dashboard/Drivers/UpdateDriverRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Dashboard\Drivers;
 
 use App\Models\User;
+use App\Traits\Files;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules\Password;
 
@@ -25,18 +26,20 @@ class UpdateDriverRequest extends FormRequest
     {
         return [
             'username' => ['string', 'between:3,15', 'unique:users,username,' . request()->driver->user_id . ',id'],
-            'place_of_residence' => ['string', 'between:3,50'],
             'work_status' => ['string', 'in:سائق حر,يعمل ضمن برنامج توصيل,لا يعمل ضمن أي برنامج'],
             'program_name' => ['required_if:work_status,يعمل ضمن برنامج توصيل', 'string', 'between:3,50'],
-            'age' => ['integer', 'between:18,45'],
+            'birth_date' => ['date'],
             'number' => ['string', 'between:3,15'],
             'nationality' => ['string', 'in:سوري,مصري'],
             'gender' => ['string', 'in:انثى,ذكر'],
-            'car_name' => ['string', 'between:3,50'],
+            'car_model' => ['string', 'between:3,50'],
             'car_color' => ['string', 'between:3,15'],
             'car_number' => ['string', 'between:3,50'],
             'car_year' => ['integer', 'digits:4'],
-            'details_file' => ['file', 'mimes:pdf', 'max:4096'],
+            'personal_id_image' => ['image', 'mimes:png,jpg', 'max:4096'],
+            'driving_license_image' => ['image', 'mimes:png,jpg', 'max:4096'],
+            'car_mechanics_image' => ['image', 'mimes:png,jpg', 'max:4096'],
+            'image' => ['image', 'mimes:png,jpg', 'max:4096'],
         ];
     }
 
@@ -45,6 +48,37 @@ class UpdateDriverRequest extends FormRequest
         $user->update([
             'username' => $this->username,
         ]);
-        $driver->update($this->except(['details_file', 'username', '_method']));
+        if($this->file('image')) {
+            if($user->image) {
+                Files::deleteFile(public_path("Images/{$user->id}/{$user->image}"));
+            }
+            $new_image = Files::moveFile($this->image, "Images/{$user->id}");
+            $user->update(['image' => $new_image]);
+        }
+
+        if($this->file('personal_id_image')) {
+            Files::deleteFile(public_path("Drivers_Images/{$user->driver->personal_id_image}"));
+            $new_image = Files::moveFile($this->personal_id_image, "Drivers_Images");
+            $user->driver()->update(['personal_id_image' => $new_image]);
+        }
+
+        if($this->file('driving_license_image')) {
+            Files::deleteFile(public_path("Drivers_Images/{$user->driver->driving_license_image}"));
+            $new_image = Files::moveFile($this->driving_license_image, "Drivers_Images");
+            $user->driver()->update(['driving_license_image' => $new_image]);
+        }
+
+        if($this->file('car_mechanics_image')) {
+            Files::deleteFile(public_path("Drivers_Images/{$user->driver->car_mechanics_image}"));
+            $new_image = Files::moveFile($this->car_mechanics_image, "Drivers_Images");
+            $user->driver()->update(['car_mechanics_image' => $new_image]);
+        }
+
+        if ($this->work_status == 'يعمل ضمن برنامج توصيل') {
+            $this->merge(['work_status' => $this->work_status . ', ' . $this->program_name]);
+        }
+
+        $driver->update($this->except(['program_name', 'car_mechanics_image', 'driving_license_image', 'personal_id_image', 'image', 'username', '_method']));
+
     }
 }
